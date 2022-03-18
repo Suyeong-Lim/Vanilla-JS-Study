07ba51e754f0a655976cf592b5f58b5920a2ee71
SY <oowwo@naver.com>
2022-03-19 00:02:58 +0900

    Add: 검색폼 추가

--- Section01_Search_Form/search-form/js/main.js
+++ Section01_Search_Form/search-form/js/main.js
@@ -1,8 +1,16 @@
-import Store from "./Store";
-import storage from "./storage";
+import Controller from "./Controller.js";
+import Store from "./Store.js";
+import storage from "./storage.js";
+import SearchFormView from "./views/SearchFormView.js";
+
+const tag = "[main]";
+document.addEventListener("DOMContentLoaded", main);
 
 function main() {
-  const store = new Store(Storage);
-  const Views = {};
-  new Controller(store, Views);
+  console.log(tag, "main");
+  const store = new Store(storage);
+  const views = {
+    searchFormView: new SearchFormView(),
+  };
+  new Controller(store, views);
 }
--- Section01_Search_Form/search-form/js/views/View.js
+++ Section01_Search_Form/search-form/js/views/View.js
@@ -1,1 +1,10 @@
 const tag = "[View]";
+
+export default class View {
+  constructor(element) {
+    if (!element) throw "no element";
+
+    this.element = element;
+    return this;
+  }
+}

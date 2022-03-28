5577fe7627bec249bb8ee4d91bdebcce567a0dac
SY <oowwo@naver.com>
2022-03-28 23:41:42 +0900

    Add:검색키워드 기능

--- Section02_Cat_Search/cat-search/src/js/components/SearchInput.js
+++ Section02_Cat_Search/cat-search/src/js/components/SearchInput.js
@@ -1,21 +1,76 @@
 const TEMPLATE = '<input type="text">';
 
 export default class SearchInput {
-  constructor({ $target, onSearch }) {
+  constructor({ $target, onSearch, keywords }) {
     const $searchInput = document.createElement("input");
+    const $searchKeywords = document.createElement("section");
+
     this.$searchInput = $searchInput;
-    this.$searchInput.placeholder = "고양이를 검색해보세요.|";
+    this.$searchInput.placeholder = "고양이를 검색해보세요.";
+    this.$searchInput.autofocus = true;
+
+    this.$searchKeywords = $searchKeywords;
+
+    this.keywords = keywords;
+
+    this.template = new Template();
+    this.onSearch = onSearch;
 
     $searchInput.className = "SearchInput";
     $target.appendChild($searchInput);
+    $target.appendChild($searchKeywords);
 
     $searchInput.addEventListener("keyup", (e) => {
+      const inputvalue = e.target.value;
       if (e.keyCode === 13) {
-        onSearch(e.target.value);
+        onSearch(inputvalue);
+        this.addKeyword(inputvalue);
       }
     });
 
-    console.log("SearchInput created.", this);
+    $searchInput.addEventListener("click", (e) => {
+      if (e.target.value) {
+        e.target.value = "";
+      }
+    });
+
+    this.render();
   }
-  render() {}
+
+  addKeyword(keyword) {
+    if (this.keywords.length >= 5) {
+      this.keywords.shift();
+    }
+    this.keywords.push(keyword);
+    this.render(this.keywords);
+  }
+
+  render(data = []) {
+    this.$searchKeywords.innerHTML = "";
+    if (data) {
+      console.log("Alldata", data);
+      this.$searchKeywords.innerHTML = this.template.getList(data);
+
+      this.$searchKeywords.querySelectorAll(".keyword").forEach(($item) => {
+        $item.addEventListener("click", (e) => {
+          const inputKeyword = e.target.dataset.keyword;
+          this.onSearch(inputKeyword);
+          this.$searchInput.value = inputKeyword;
+        });
+      });
+    }
+  }
+}
+
+class Template {
+  getList(data = []) {
+    return `
+    <ul>${data
+      .map(
+        (word, id) =>
+          `<li class="keyword" id="${id}" data-keyword="${word}">${word}</li>`
+      )
+      .join("")}</ul>
+    `;
+  }
 }
